73c5e8f919d058eb45ca9557ec690da2d955f860
juanyeffrey <124712883+juanyeffrey@users.noreply.github.com>
2025-11-20 15:29:57 -0600

    Light Diffusion Models

--- diffusion/config.py
+++ diffusion/config.py
@@ -3,7 +3,7 @@
 import torch
 
 # You can switch to SDXL or any other model later
-SD_MODEL_ID = "runwayml/stable-diffusion-v1-5"
+SD_MODEL_ID = "stabilityai/sd-turbo" #"stabilityai/sd-turbo" #"runwayml/stable-diffusion-v1-5"
 
 # Base prompt used as semantic “anchor”
 BASE_PROMPT = "an abstract generative painting"
@@ -16,6 +16,6 @@ STYLE_STRENGTH = 0.8  # you can tweak this
 
 # Default diffusion parameters
 GUIDANCE_SCALE = 7.5
-NUM_INFERENCE_STEPS = 30
+NUM_INFERENCE_STEPS = 5
 IMAGE_HEIGHT = 512
 IMAGE_WIDTH = 512
